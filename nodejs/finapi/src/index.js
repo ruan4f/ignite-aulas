777c3968821237db957ef743be5c82a2c2d8ef4a
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-01-29 14:31:48 -0300

    Changes

--- nodejs/finapi/src/index.js
+++ nodejs/finapi/src/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const {
+  v4: uuidv4
+} = require('uuid');
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
+
+app.post('/account', (req, res) => {
+  const { cpf,name } = req.body;
+  const customersAlreadyExist = customers.some(customer => customer.cpf === cpf);
+
+  if (customersAlreadyExist) {
+    return res.status(400).json({ error: 'Customer already exists'});
+  } 
+  
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return res.status(201).send();
+});
 
 app.listen('3333');
