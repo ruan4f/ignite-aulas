8518521b63c5563e7005333e4d4c2911ca77c2ab
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-01-29 01:21:59 -0300

    Update index.js

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (req, res) => {
+  const query = req.query;
+  console.log(query);
   return res.json(['Curso 1', 'Curso 2']);
 });
 
 app.post('/courses', (req, res) => {
+  const body = req.body;
+  console.log(body);
   return res.json(['Curso 1', 'Curso 2']);
 });
 
 app.put('/courses/:id', (req, res) => {
-  const params = req.params;
-  console.log(params);
+  const {id} = req.params;
+  console.log(id);
   return res.json(['Curso 6', 'Curso 2']);
 });
 
