1b1205acd226971e8dbab5949f47f6c59822ff6e
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-01-30 23:38:15 -0300

    Update index.js

--- nodejs/finapi/src/index.js
+++ nodejs/finapi/src/index.js
@@ -31,4 +31,10 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+});
+
 app.listen('3333');
