061c14c17588e599d42cedf25107bec5b0cbae73
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-01 23:45:59 -0300

    Changes

--- nodejs/finapi/README.md
+++ nodejs/finapi/README.md
@@ -10,7 +10,7 @@ Requisitos
 [] Deve ser possível deletar uma conta
 Regras de negócio
 [x] Não deve ser possível cadastrar uma conta com CPF já existente
-[] Não deve ser possível fazer depósito em uma conta não existente
+[x] Não deve ser possível fazer depósito em uma conta não existente
 [] Não deve ser possível buscar extrato em uma conta não existente
 [] Não deve ser possível fazer saque em uma conta não existente
 [] Não deve ser possível excluir uma conta não existente
--- nodejs/finapi/src/index.js
+++ nodejs/finapi/src/index.js
@@ -6,6 +6,20 @@ const app = express();
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Constumer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.use(express.json());
 
 app.post('/account', (req, res) => {
@@ -31,14 +45,10 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement', (req, res) => {
-  const { cpf } = req.headers;
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ error: "Constumer not found" });
-  }
-
+//app.use(verifyIfExistsAccountCPF);
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {  
+  const { customer } = req;
+  
   return res.json(customer.statement);
 });
 
