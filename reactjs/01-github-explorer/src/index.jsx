11bffa63820f4522e864cfb251a9c849564fe7c9
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-06 14:51:19 -0300

    Config

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,6 +1,5 @@
 import { render } from "react-dom";
-import React from "react";
 import { App } from "./App";
 
-render(<h1>Hello</h1>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
