33543a83803623dc0ff60a6b5c3ee642646cde3b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-06 15:12:45 -0300

    Update webpack.config.js

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'eval-source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,6 +11,11 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  devServer:{
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
