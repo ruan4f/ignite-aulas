98830cfeb747e9a6d3b4624c7b256a94110f41a3
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-07 22:04:01 -0300

    Config typescript

--- reactjs/01-github-explorer/src/components/RepositoryItem.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -1,6 +1,12 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string,
+    description: string,
+    html_url: string
+  }
+}
 
-
-export function RepositoryItem(props) {
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
