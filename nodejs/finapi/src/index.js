d2933254a4c029b465c931cf78af2cf5266015eb
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-09 13:33:59 -0300

    Update index.js

--- nodejs/finapi/src/index.js
+++ nodejs/finapi/src/index.js
@@ -115,6 +115,24 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
 app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
   const { name } = request.body;
   const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {  
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {  
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
 });
 
 app.listen("3333");
