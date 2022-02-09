e20b5ae5dd7b62b110c410836863752a5db53fe0
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-08 22:22:13 -0300

    Update global.ts

--- reactjs/dtmoney/src/styles/global.ts
+++ reactjs/dtmoney/src/styles/global.ts
@@ -3,6 +3,16 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
   
   * {
@@ -25,5 +35,14 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background:var(--background);
     -webkit-font-smoothing:antialiased;
+  }
+
+  button {
+    cursor: pointer;    
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `;
