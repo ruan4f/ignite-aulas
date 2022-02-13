bff98bef5b795ce366dc551bb3618493691f773f
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-12 23:42:30 -0300

    Update styles.ts

--- reactjs/dtmoney/src/components/Header/styles.ts
+++ reactjs/dtmoney/src/components/Header/styles.ts
@@ -8,5 +8,22 @@ export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
 
-  padding: 0 1rem 10rem;
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button { 
+    font-size: 1rem;
+    color: #FFF;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
