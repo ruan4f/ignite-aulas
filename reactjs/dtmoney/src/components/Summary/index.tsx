9e3fbf9cf28e3fdfb6e311500120e66fc39f0ec8
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-13 15:36:17 -0300

    Summary layout

--- reactjs/dtmoney/src/components/Summary/index.tsx
+++ reactjs/dtmoney/src/components/Summary/index.tsx
@@ -23,7 +23,7 @@ export function Summary(){
         <strong>- R$ 500,00</strong>
       </div>
 
-      <div>
+      <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
