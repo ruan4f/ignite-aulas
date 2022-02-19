b77a9162abf09375e2d59766bb6c6f0a2c7b9943
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-19 11:57:01 -0300

    Styled component

--- reactjs/dtmoney/src/components/NewTransactionModal/styles.ts
+++ reactjs/dtmoney/src/components/NewTransactionModal/styles.ts
@@ -56,12 +56,16 @@ export const TransactionTypeContainer = styled.div`
   gap: 0.5rem;
 `;
 
-export const RadioBox = styled.button`
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: transparent;
+  background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
 
   display: flex;
   align-items: center;
