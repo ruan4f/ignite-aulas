f852c29b332d3d7f094afdb25a2f13788f50da1d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-03-08 07:48:11 -0300

    Configure _document

---
+++ reactjs/ignews/src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/Document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+
+          <title>ig.news</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
