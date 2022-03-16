fd07c289e967429edaab26f7f615d4f8cd03a319
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-03-16 20:52:59 -0300

    Update index.tsx

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <h1 className={styles.title}>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+
+      
       Hello World
     </h1>
   );
