31dd00738c09ccca5fc826e917f184bd0d38e963
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-03-16 21:09:43 -0300

    Update dynamics title

--- reactjs/ignews/src/pages/_document.tsx
+++ reactjs/ignews/src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
 
-          <title>ig.news</title>
+          
         </Head>
         <body>
           <Main />
--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -3,13 +3,11 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-
-      
-      Hello World
-    </h1>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   );
 }
