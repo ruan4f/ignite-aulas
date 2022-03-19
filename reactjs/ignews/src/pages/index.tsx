3be429d8280bc6b18bf2667e12d0c15064373354
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-03-19 00:16:23 -0300

    New components

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -7,9 +10,15 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
       
-      <main>
-        <section>
-          <span>Hey, Welcome</span>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
         </section>
 
         <img src="images/avatar.svg" alt="Girl coding" />
