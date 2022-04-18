3fff6bfad03e6bca4ecbfd4ba191eaebb871da77
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-04-18 17:10:30 -0300

    Changes

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -5,7 +5,14 @@ import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,9 +27,9 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="images/avatar.svg" alt="Girl coding" />
@@ -33,17 +40,20 @@ export default function Home() {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const price = await stripe.prices.retrieve('price_1KesphD5rFsAbTbeCUYOS0OB', {
-    expand: ['product']
+    expand: ['product'],
   });
 
   const product = {
     priceId: price.id,
-    amount: price.unit_amount / 100,
-  }
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
 
   return {
     props: {
-      product
+      product,
     },
   };
 };
