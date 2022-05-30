0695155e07b0f35dce525011a96c13caf2773fef
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-05-30 19:35:11 -0300

    Update [...nextauth].ts

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -18,7 +18,13 @@ export default NextAuth({
       const { email } = user;
 
       try {
-        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
         return true;
       } catch {
         return false;
