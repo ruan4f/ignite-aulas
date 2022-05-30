035ae1f4e68b7c1965e69ab99d1291daec8ca496
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-05-30 19:27:11 -0300

    Configuration create user in faunadb

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,28 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
