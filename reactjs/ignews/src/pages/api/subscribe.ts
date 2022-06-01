4bb6e50766f5592188acfb6f7140c9eb9627d2dd
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-05-31 21:15:40 -0300

    Update subscribe.ts

--- reactjs/ignews/src/pages/api/subscribe.ts
+++ reactjs/ignews/src/pages/api/subscribe.ts
@@ -1,28 +1,64 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { query as q } from 'faunadb';
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 const subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      //metadata
-    });
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(q.Index('user_by_email'), q.Casefold(session.user.email))
+      )
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: { stripe_customer_id: stripeCustomer.id },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
-      line_items: [{ price: 'price_1KesphD5rFsAbTbeCUYOS0OB', quantity: 1 }],
+      line_items: [
+        {
+          price: 'price_1KesphD5rFsAbTbeCUYOS0OB',
+          quantity: 1,
+        },
+      ],
       mode: 'subscription',
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+    return res.status(200).json({
+      sessionId: stripeCheckoutSession.id,
+    });
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).send('Method not allowed');
