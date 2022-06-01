fc334b83ac1a39dd7fc4a7f97593e4581931ef7f
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-05-31 22:47:59 -0300

    Configuration webhooks  stripe

--- reactjs/ignews/README.md
+++ reactjs/ignews/README.md
@@ -32,3 +32,7 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Execute Stripe in localhost
+
+stripe listen --forward-to localhost:3000/api/webhooks
---
+++ reactjs/ignews/src/pages/api/webhooks.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set(['checkout.session.completed']);
+
+const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const secret = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      res.status(404).send(`Webhook error: ${err.mensage}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      console.log('evento recebido', event);
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).send('Method not allowed');
+  }
+};
+
+export default webhooks;
