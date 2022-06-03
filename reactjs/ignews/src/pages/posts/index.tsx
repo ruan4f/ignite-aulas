cc69e9948e38a8b093ab16eba0ae4560c3b29ea2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-02 22:43:15 -0300

    Page posts

---
+++ reactjs/ignews/src/pages/posts/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lern</strong>
+            <p>in this</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
