06d203a805cf66dd3612dc933ca6bd5edabbae73
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-07 20:51:32 -0300

    CSS PreviewPost

--- reactjs/ignews/src/pages/posts/preview/[slug].tsx
+++ reactjs/ignews/src/pages/posts/preview/[slug].tsx
@@ -25,7 +25,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
           <div
-            className={styles.postContent}
+            className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
         </article>
@@ -34,9 +34,15 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
-
 
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(slug), {});
@@ -44,7 +50,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const post = {
     slug,
     title: RichText.asText(response.data.title),
-    content: RichText.asHtml(response.data.content),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
