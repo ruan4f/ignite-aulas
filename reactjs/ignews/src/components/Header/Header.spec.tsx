435b1d5947b0bf70d0f17550e752cc4baff2a9d5
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-08-22 21:05:19 -0300

    New test

---
+++ reactjs/ignews/src/components/Header/Header.spec.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import { Header } from '.';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+});
+
+jest.mock('next-auth/react', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+});
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <Header />
+    );
+
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('Posts')).toBeInTheDocument();
+  });
+});
