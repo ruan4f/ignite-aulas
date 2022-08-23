12c6253d0df53b2ac0bd838270d28e4157eafaeb
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-08-22 21:06:47 -0300

    Simplify the test

--- reactjs/ignews/src/components/Header/Header.spec.tsx
+++ reactjs/ignews/src/components/Header/Header.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Header } from '.';
 
 jest.mock('next/router', () => {
@@ -21,11 +21,11 @@ jest.mock('next-auth/react', () => {
 
 describe('Header component', () => {
   it('renders correctly', () => {
-    const { getByText } = render(
+    render(
       <Header />
     );
 
-    expect(getByText('Home')).toBeInTheDocument();
-    expect(getByText('Posts')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Posts')).toBeInTheDocument();
   });
 });
