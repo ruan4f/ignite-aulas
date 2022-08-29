4e9af27f722f23b5c9ec1732b5d38a7ba3b2f5d2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-08-28 22:01:27 -0300

    Update SubscribeButton.spec.tsx

--- reactjs/ignews/src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ reactjs/ignews/src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { signIn } from 'next-auth/react';
 import { SubscribeButton } from '.';
 
 jest.mock('next-auth/react', () => {
   return {
     useSession() {
       return [null, false];
-    }
+    },
+    signIn: jest.fn(),
   }
 });
 
@@ -20,11 +22,23 @@ jest.mock('next/router', () => {
 });
 
 describe('SubscribeButton component', () => {
-  it('renders correctly', () => {   
+  it('renders correctly', () => {
 
     render(<SubscribeButton />);
 
     expect(screen.getByText('Subscribe Now')).toBeInTheDocument();
+  });
+
+  it('redirects user to sign in when not authenticated', () => {
+    const signInMocked = (signIn as jest.Mock);
+
+    render(<SubscribeButton />);
+
+    const subscriptionButton = screen.getByText('Subscribe Now');
+
+    fireEvent.click(subscriptionButton);
+
+    expect(signInMocked).toHaveBeenCalled();
   });
 
 });
