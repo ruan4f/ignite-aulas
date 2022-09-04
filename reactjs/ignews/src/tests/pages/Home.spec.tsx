0bd9f4dc2758df70a4786f3616c395bc6c0ba8ca
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-04 13:41:33 -0300

    Update Home.spec.tsx

--- reactjs/ignews/src/tests/pages/Home.spec.tsx
+++ reactjs/ignews/src/tests/pages/Home.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import Home, { getStaticProps } from '../../pages';
+import { stripe } from '../../services/stripe';
+
 
 jest.mock('next/router');
 jest.mock('next-auth/react', () => {
@@ -7,6 +9,7 @@ jest.mock('next-auth/react', () => {
     useSession: () => [null, false]
   }
 });
+jest.mock('../../services/stripe');
 
 describe('Home Page', () => {
   it('renders correctly', () => {
@@ -15,4 +18,25 @@ describe('Home Page', () => {
     expect(screen.getByText('for R$10,00 month')).toBeInTheDocument();
   });
 
+  it('loads initial data', async () => {
+    const retrieveStripePricesMocked = (stripe.prices.retrieve as jest.Mock);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-prices-id',
+      unit_amount: 1000,
+    });
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-prices-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    );
+  });
 });
