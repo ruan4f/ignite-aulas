084fa8294d348919ecb6162eff83819e7041c4ab
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-07 16:30:42 -0300

    Create Async.spec.tsx

---
+++ reactjs/ignews/src/components/Async/Async.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { Async } from '.';
+
+
+test('it renders correctly', async () => {
+  render(<Async/>);
+
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
+  expect(await screen.findByText('Button')).toBeInTheDocument();
+
+  await waitFor(() => {
+    return expect(screen.getByText('Button')).toBeInTheDocument();
+  })
+});
