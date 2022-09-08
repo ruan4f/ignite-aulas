7cc5b5fb08932b676c6204dd9fc4118674abc8be
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-09-07 22:49:24 -0300

    More test

--- reactjs/ignews/src/components/Async/Async.spec.tsx
+++ reactjs/ignews/src/components/Async/Async.spec.tsx
@@ -1,14 +1,13 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { Async } from '.';
 
-
 test('it renders correctly', async () => {
   render(<Async/>);
 
   expect(screen.getByText('Hello World')).toBeInTheDocument();
-  expect(await screen.findByText('Button')).toBeInTheDocument();
+  //expect(await screen.findByText('Button')).toBeInTheDocument();
 
   await waitFor(() => {
-    return expect(screen.getByText('Button')).toBeInTheDocument();
+    return expect(screen.queryByText('Button')).not.toBeInTheDocument();
   })
 });
--- reactjs/ignews/src/components/Async/index.tsx
+++ reactjs/ignews/src/components/Async/index.tsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from 'react';
 
 export function Async() {
   const [isButtonVisible, setIsButtonVisible] = useState(false);
+  const [isButtonInvisible, setIsButtonInvisible] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsButtonVisible(true)
+      setIsButtonInvisible(true)
     }, 1000)
   }, []);
   
   return (
     <div>
-      <div>Hello World</div>{isButtonVisible && <button>Button</button> }
+      <div>Hello World</div>{!isButtonInvisible && <button>Button</button> }
     </div>
     )
 }
