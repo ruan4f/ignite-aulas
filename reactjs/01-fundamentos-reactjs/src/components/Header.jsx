0644a32e9ea2028345c50005999d729f7d0a0cc7
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-13 23:34:41 -0300

    Update Header.jsx

--- reactjs/01-fundamentos-reactjs/src/components/Header.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
