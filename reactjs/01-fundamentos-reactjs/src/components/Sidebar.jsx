14a20ddff91a9b6d7c189161aa7e06e5f4349cbb
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-23 21:17:10 -0300

    Study react

--- reactjs/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +10,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/ruan4f.png" />
+
         <strong>Ruan Ferreira</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu Perfil
         </a>
       </footer>
