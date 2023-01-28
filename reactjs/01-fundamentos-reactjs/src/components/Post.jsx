83fd8a09f0fac0f9394116da6000d54860e55998
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-27 22:35:00 -0300

    Css post form

--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -25,6 +25,16 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu Feedback</strong>
+
+        <textarea placeholder='Deixe seu comentário' />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
