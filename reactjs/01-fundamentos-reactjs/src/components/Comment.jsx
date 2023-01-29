9f7af1ec40bf4100ed0daebd2cfe50aecc307f57
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-28 21:15:25 -0300

    New comment component

---
+++ reactjs/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ruan4f.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Ruan Ferreira</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir<span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
