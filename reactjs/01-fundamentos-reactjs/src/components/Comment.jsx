0a78359575b9c88e88b3891bfe9c6f1345ecf868
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-30 23:21:36 -0300

    New styles avatar component

--- reactjs/01-fundamentos-reactjs/src/components/Comment.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/ruan4f.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/ruan4f.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/ruan4f.png" />
+          <Avatar src="https://github.com/ruan4f.png" />
           <div className={styles.authorInfo}>
             <strong>Ruan Ferreira</strong>
             <span>Web Developer</span>
