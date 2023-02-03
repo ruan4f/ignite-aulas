b52a901ad8110d988de83a9ed9452cbbbe1ef1c1
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-02 22:41:55 -0300

    Os motores do React - Entendendo a key

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -79,6 +79,7 @@ function App() {
           {posts.map(post => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
