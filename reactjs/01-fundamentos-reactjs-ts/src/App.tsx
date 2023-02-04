d3dfb323ffb0e9c4b682b1bd9ca62a25205c45c4
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-04 15:33:34 -0300

    Conversion project to ts

---
+++ reactjs/01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,95 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ruan4f.png',
+      name: 'Ruan Ferreira',
+      role: 'Web Developer'
+    },
+    content: [
+      {
+        type: 'paragraph', content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link', content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {
+        type: 'paragraph', content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link', content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {
+        type: 'paragraph', content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link', content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-05-06 20:00:00')
+  }
+]
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ reactjs/01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1599837565318-67429bde7162?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/ruan4f.png" />
+
+        <strong>Ruan Ferreira</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
