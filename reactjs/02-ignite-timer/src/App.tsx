6c93068f00a5322888b94ba532b55dce20fad9fc
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-05 19:13:36 -0300

    Estrutura da aplicação - Tipagem de temas

--- reactjs/02-ignite-timer/src/App.tsx
+++ reactjs/02-ignite-timer/src/App.tsx
@@ -1,13 +1,15 @@
-import { Button } from './components/Button';
+import { ThemeProvider } from "styled-components";
 
+import { Button } from './components/Button';
+import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
--- reactjs/02-ignite-timer/src/components/Button.styles.ts
+++ reactjs/02-ignite-timer/src/components/Button.styles.ts
@@ -16,9 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
-  }}
+  }} */
 `;
