548c044f5f9dc31d7d4aa164322a67705ba15083
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-19 19:00:47 -0300

    Funcionalidades da aplicação - Reduzindo countdown

--- reactjs/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs/02-ignite-timer/src/pages/Home/index.tsx
@@ -2,7 +2,9 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
-import { useState } from 'react'
+import { useState, useEffect, useState } from 'react'
+
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownContainer,
@@ -30,6 +32,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -45,6 +48,18 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
@@ -52,6 +67,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -59,8 +75,6 @@ export function Home() {
 
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
