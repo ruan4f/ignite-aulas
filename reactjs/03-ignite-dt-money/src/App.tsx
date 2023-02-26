b033030fdf662868e9d89a27075368cda4e7459b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-26 17:36:36 -0300

    Estrutura visual - Componente: Header

--- reactjs/03-ignite-dt-money/src/App.tsx
+++ reactjs/03-ignite-dt-money/src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Hello World</h1>
+      <Transactions />
    </ThemeProvider>
   )
 }
