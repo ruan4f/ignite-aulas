e9951335545ca7c4cf94d9fba9a5064c67a091a5
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-03 21:29:48 -0300

    Conexão com a API - Aplicando React Hook Form

--- reactjs/03-ignite-dt-money/src/components/NewTransactionModal/styles.ts
+++ reactjs/03-ignite-dt-money/src/components/NewTransactionModal/styles.ts
@@ -30,6 +30,7 @@ export const Content = styled(Dialog.Content)`
       background: ${props => props.theme["gray-900"]};
       color: ${props => props.theme["gray-300"]};
       padding: 1rem;
+      
       &::placeholder {
         color: ${props => props.theme["gray-500"]};
       }
@@ -44,7 +45,13 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 1.25rem;
       cursor: pointer;
-      &:hover {
+
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${props => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
--- reactjs/03-ignite-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
+++ reactjs/03-ignite-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
@@ -25,7 +25,14 @@ export const SearchFormContainer = styled.form`
     color: ${props => props.theme["green-300"]};
     font-weight: bold;
     border-radius: 6px;
-    &:hover {
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+    
+    &:not(:disabled):hover {
       background: ${props => props.theme["green-500"]};
       border-color: ${props => props.theme["green-500"]};
       color: ${props => props.theme["white"]};
