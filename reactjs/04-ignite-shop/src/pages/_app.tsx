181cd5f4811b249ce375c954ae400dd778f7a4a0
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-11 16:53:00 -0300

    Estrutura visual - Estilos globais

--- reactjs/04-ignite-shop/src/pages/_app.tsx
+++ reactjs/04-ignite-shop/src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-import type { AppProps } from 'next/app'
+globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default App
