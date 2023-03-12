21aca28c97337dad0b6bdc13174f77cb011c9e2d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-12 14:29:18 -0300

    Estrutura visual - Criando carrossel

--- reactjs/04-ignite-shop/src/pages/index.tsx
+++ reactjs/04-ignite-shop/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import camiseta1 from '../assets/camisetas/1.png'
@@ -6,10 +8,19 @@ import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 import camiseta4 from '../assets/camisetas/4.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -18,8 +29,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
