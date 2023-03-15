101f1dd55fc008c131590b8de75fbc3db556c148
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-15 12:35:58 -0300

    Produto & Checkout - Navegação via Link

--- reactjs/04-ignite-shop/src/pages/index.tsx
+++ reactjs/04-ignite-shop/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 import { GetStaticProps } from "next"
+import Link from "next/link"
 
 import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
@@ -29,14 +30,18 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
